6406aa77fea68afea698e666bac6aaa36cb7dc83
Kevin Sanchez <kevnsan0517@gmail.com>
2022-03-05 13:47:23 -0500

    added an API, refactoring

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const { MongoClient } = require('mongodb');
 const baby_names = "baby_names";
 const male_names = "male_names";
 const female_names = "female_names";
+const state_male_names = "state_male_names";
+const state_female_names = "state_female_names";
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.ufxrh.mongodb.net/${baby_names}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 
@@ -17,79 +19,41 @@ app.get('/', (req, res) => {
 app.get('/top_ten_names/:year', async (req, res) => {
     try {
         const year = parseInt(req.params.year);
-        const db = client.db(baby_names);
-        const female_col = db.collection(female_names);
-        const females = await female_col.aggregate(
-            [
-                {
-                    '$project': {
-                        'years': {
-                            '$filter': {
-                                'input': '$years',
-                                'as': 'val',
-                                'cond': {
-                                    '$eq': [
-                                        '$$val.year', year
-                                    ]
-                                }
+        const query = [
+            {
+                '$project': {
+                    'years': {
+                        '$filter': {
+                            'input': '$years',
+                            'as': 'val',
+                            'cond': {
+                                '$eq': [
+                                    '$$val.year', year
+                                ]
                             }
                         }
                     }
-                }, {
-                    '$unwind': {
-                        'path': '$years'
-                    }
-                }, {
-                    '$project': {
-                        'count': '$years.count',
-                        'rank': '$years.rank'
-                    }
-                }, {
-                    '$sort': {
-                        'rank': 1
-                    }
-                }, {
-                    '$limit': 10
                 }
-            ]
-        ).toArray();
-        const male_col = db.collection(male_names);
-        const males = await male_col.aggregate(
-            [
-                {
-                    '$project': {
-                        'years': {
-                            '$filter': {
-                                'input': '$years',
-                                'as': 'val',
-                                'cond': {
-                                    '$eq': [
-                                        '$$val.year', year
-                                    ]
-                                }
-                            }
-                        }
-                    }
-                }, {
-                    '$unwind': {
-                        'path': '$years'
-                    }
-                }, {
-                    '$project': {
-                        'count': '$years.count',
-                        'rank': '$years.rank'
-                    }
-                }, {
-                    '$sort': {
-                        'rank': 1
-                    }
-                }, {
-                    '$limit': 10
+            }, {
+                '$unwind': {
+                    'path': '$years'
                 }
-            ]
-        ).toArray();
+            }, {
+                '$project': {
+                    'count': '$years.count',
+                    'rank': '$years.rank'
+                }
+            }, {
+                '$sort': {
+                    'rank': 1
+                }
+            }, {
+                '$limit': 10
+            }
+        ];
+        const result = await retrieveTwoCols(female_names, male_names, query);
         res.header("Content-Type", 'application/json');
-        res.send(JSON.stringify({ females, males }, null, 4));
+        res.send(JSON.stringify(result, null, 4));
     } catch (err) {
         res.send(`Failed to retrieve data:\n${err.stack}`);
     }
@@ -101,48 +65,105 @@ app.get('/name_range/:sex/:name/:startYear/:endYear', async (req, res) => {
         const name = req.params.name;
         const startYear = parseInt(req.params.startYear);
         const endYear = parseInt(req.params.endYear);
-        const db = client.db(baby_names);
-        const col = db.collection(sex === "F" ? female_names : male_names);
-        const arr = await col.aggregate(
-            [
-                {
-                    '$match': {
-                        '_id': name
-                    }
-                }, {
-                    '$unset': '_id'
-                }, {
-                    '$unwind': {
-                        'path': '$years'
-                    }
-                }, {
-                    '$project': {
-                        'year': '$years.year',
-                        'rank': '$years.rank',
-                        'count': '$years.count'
-                    }
-                }, {
-                    '$match': {
-                        'year': {
-                            '$gte': startYear,
-                            '$lte': endYear
-                        }
-                    }
-                }, {
-                    '$sort': {
-                        'year': -1
+        const query = [
+            {
+                '$match': {
+                    '_id': name
+                }
+            }, {
+                '$unset': '_id'
+            }, {
+                '$unwind': {
+                    'path': '$years'
+                }
+            }, {
+                '$project': {
+                    'year': '$years.year',
+                    'rank': '$years.rank',
+                    'count': '$years.count'
+                }
+            }, {
+                '$match': {
+                    'year': {
+                        '$gte': startYear,
+                        '$lte': endYear
                     }
                 }
-            ]
-        ).toArray();
+            }, {
+                '$sort': {
+                    'year': -1
+                }
+            }
+        ]
+        const result = await retrieveOneCol(sex === "F" ? female_names : male_names, query);
         res.header("Content-Type", 'application/json');
-        res.send(JSON.stringify(arr, null, 4));
+        res.send(JSON.stringify(result, null, 4));
     } catch (err) {
         res.send(`Failed to retrieve data:\n${err.stack}`);
     }
 });
 
+app.get('/top_ten_names_state/:state/:year', async (req, res) => {
+    try {
+        const state = req.params.state;
+        const year = parseInt(req.params.year);
+        const query = [
+            {
+                '$project': {
+                    'state': `$state.${state}`
+                }
+            }, {
+                '$project': {
+                    'years': {
+                        '$filter': {
+                            'input': '$state',
+                            'as': 'y',
+                            'cond': {
+                                '$eq': [
+                                    '$$y.year', year
+                                ]
+                            }
+                        }
+                    }
+                }
+            }, {
+                '$unwind': {
+                    'path': '$years'
+                }
+            }, {
+                '$project': {
+                    'count': '$years.count',
+                    'rank': '$years.rank'
+                }
+            }, {
+                '$sort': {
+                    'rank': 1
+                }
+            }, {
+                '$limit': 10
+            }
+        ];
+        console.log(query);
+        const result = await retrieveTwoCols(state_female_names, state_male_names, query);
+        res.header("Content-Type", 'application/json');
+        res.send(JSON.stringify(result, null, 4));
+    } catch (err) {
+        res.send(`Failed to retrieve data:\n${err.stack}`);
+    }
+});
 
+async function retrieveTwoCols(female_col, male_col, query) {
+    const db = client.db(baby_names);
+    const females = await db.collection(female_col).aggregate(query).toArray();
+    const males = await db.collection(male_col).aggregate(query).toArray();
+    return { females, males };
+}
+
+async function retrieveOneCol(col, query) {
+    const db = client.db(baby_names);
+    const names = await db.collection(col).aggregate(query).toArray();
+    return names;
+}
 
 app.listen(port, async () => {
     console.log(`App now listening at port:${port}`);
