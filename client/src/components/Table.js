d052d08af5ddbf3b66b74fd9221f37f2a1f9e947
Kevin Sanchez <kevnsan0517@gmail.com>
2022-03-08 19:57:10 -0500

    return blank html if body prop is null

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,18 +1,21 @@
 import TableRow from "./TableRow"
 
-const Table = ({heading, body}) => {
-  return (
-    <table style={{ width: 500 }} className="table-c">
-        <thead>
-          <tr>
-            {heading.map((head, i) => <th key={i} bgcolor={head.color}>{head.label}</th>)}
-          </tr>
-        </thead>
-        <tbody>
-          {body.map((row, i) => <TableRow row={row} key={i} />)}
-        </tbody>
-      </table>
-  )
+const Table = ({ heading, body }) => {
+    if (body === null) {
+        return <></>
+    }
+    return (
+        <table style={{ width: 500 }} className="table-c">
+            <thead>
+                <tr>
+                    {heading.map((head, i) => <th key={i} bgcolor={head.color}>{head.label}</th>)}
+                </tr>
+            </thead>
+            <tbody>
+                {body.map((row, i) => <TableRow row={row} key={i} />)}
+            </tbody>
+        </table>
+    )
 }
 
 export default Table
