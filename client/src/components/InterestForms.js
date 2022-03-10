53fd1e242c563291f4eaeb1bbfedf276b22a6b54
Kevin Sanchez <kevnsan0517@gmail.com>
2022-03-10 03:46:15 -0500

    added div in InterestForms, updated css

--- client/src/components/InterestForms.js
+++ client/src/components/InterestForms.js
@@ -7,17 +7,19 @@ const InterestForms = () => {
                 <h3 className='header-c'>
                     <span className='header-c-text'>Items of Interest</span>
                 </h3>
-                <div>
-                    <h3>Popular Names by Birth Year</h3>
-                    <p>Enter the Year and Popularity for a List of the Most Popular Names</p>
-                    <p>Any year after 1879</p>
-                    <form name="popnames" >
-                        {/* <p>
-                  <input style="width:100px" type="text" pattern="\d{4}" name="year" size="5" maxlength="4" id="year" value="2020" title="Birth Year: Must be 4 numbers" required=""/>
-                  <label style="display:inline;" for="year">&nbsp;&nbsp;Birth Year</label><br/>
-                </p> */}
+                <div className='interest-div'>
+                    <div className='interest-form'>
+                        <h3>Popular Names by Birth Year</h3>
+                        <p>Enter the Year and Popularity for a List of the Most Popular Names</p>
+                        <p>Any year after 1879</p>
 
-                    </form>
+                    </div>
+                    <div className='interest-form'>
+                        <h3>Popularity of a Name</h3>
+                        <p>See How the Popularity of a Name has Changed Over Time!</p>
+                        
+
+                    </div>
                 </div>
             </div>
         </section>
