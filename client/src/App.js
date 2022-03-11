0fbe79d0f4c4e5944df263571354ed4b71b8b061
Kevin Sanchez <kevnsan0517@gmail.com>
2022-03-10 19:09:28 -0500

    moved TopNamesTable to its own folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import InterestForms from './components/InterestForms/InterestForms';
 import IntroSection from './components/IntroSection';
 import PopularityCategories from './components/PopularityCategories/PopularityCategories';
-import TopNamesTable from './components/TopNamesTable';
+import TopNamesTable from './components/TopNamesTable/TopNamesTable';
 
 function App() {
   const github = <a className='a-top' href='https://github.com/knsanchez0001/babynames'>GitHub Code</a>;
