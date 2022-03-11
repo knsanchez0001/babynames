fc181017bb0b824c6c8c59ad902b63581c3ce04e
Kevin Sanchez <kevnsan0517@gmail.com>
2022-03-11 00:14:39 -0500

    created a table for popularity of name

--- client/src/components/InterestForms/NamePopularity.js
+++ client/src/components/InterestForms/NamePopularity.js
@@ -1,20 +1,48 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom';
 import IntroSection from '../IntroSection';
+import Table from '../Table';
 
 const NamePopularity = () => {
-    const {name, start, sex} = useParams();
+    const { name, start, sex } = useParams();
     const back = <Link className='a-top' to={'/'}>Back</Link>;
     console.log(name);
     console.log(start);
     console.log(sex);
+
+    let [tableBody, setTableBody] = useState(null);
+
+    useEffect(() => {
+        const getBabynames = async () => {
+            const response = await fetch(`/name_range/${sex}/${name}/${start}/2020`);
+            if (response.ok) {
+                const json = JSON.parse(await response.text());
+
+                const body = [];
+                for (let i = 0; i < json.length; i++) {
+                    body.push([json[i].year.toString(), json[i].rank.toString(), json[i].count.toLocaleString('en')]);
+                }
+                setTableBody(body);
+            }
+        };
+
+        getBabynames();
+    }, [sex, name, start]);
+
+    const tableHeading = [
+        { label: 'Year', color: '#eeeeee' },
+        { label: 'Rank', color: '#eeeeee' }, { label: 'Count', color: '#eeeeee' }];
+
     return (
-      <>
-        <IntroSection child={back} />
-        <section>
-        
-        </section>
-      </>
+        <>
+            <IntroSection child={back} />
+            <section>
+                <h3 className='header-c'>
+                    <span className='header-c-text'>Popularity of name {name}</span>
+                </h3>
+                <Table heading={tableHeading} body={tableBody} />
+            </section>
+        </>
     )
 }
 
