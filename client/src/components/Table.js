5a0e712f27e8be8529af8c5893c1b7fcc66e8a0f
Kevin Sanchez <kevnsan0517@gmail.com>
2022-03-12 02:33:00 -0500

    updated Table.js to include an optional header

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -2,27 +2,35 @@ import TableRow from './TableRow';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Table = ({ heading, body }) => {
+const Table = ({ heading, body, tableHeader }) => {
 	if (body === null) {
 		return <></>;
 	}
 	return (
-		<table className="table-c">
-			<thead>
-				<tr>
-					{heading.map((head, i) => <th key={i} bgcolor={head.color}>{head.label}</th>)}
-				</tr>
-			</thead>
-			<tbody>
-				{body.map((row, i) => <TableRow row={row} key={i} />)}
-			</tbody>
-		</table>
+		<>
+			{tableHeader}
+			<table className="table-c">
+				<thead>
+					<tr>
+						{heading.map((head, i) => <th key={i} bgcolor={head.color}>{head.label}</th>)}
+					</tr>
+				</thead>
+				<tbody>
+					{body.map((row, i) => <TableRow row={row} key={i} />)}
+				</tbody>
+			</table>
+		</>
 	);
+};
+
+Table.defaultProps = {
+	tableHeader: <></>
 };
 
 Table.propTypes = {
 	heading: PropTypes.array.isRequired,
-	body: PropTypes.array
+	body: PropTypes.array,
+	tableHeader: PropTypes.object
 };
 
 export default Table;
